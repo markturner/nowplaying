70d314a453e6146c23d9805369c429932df1a87e
Mark Turner <markjturner@gmail.com>
2011-01-15 14:58:27 +0000

    It's alive!

--- Gemfile
+++ Gemfile
@@ -3,4 +3,8 @@ source 'http://rubygems.org/'
 gem 'sinatra', :git => 'git://github.com/sinatra/sinatra.git'
 
 gem 'bundler'
-gem 'scrobbler', git://github.com/jnunemaker/scrobbler.git
+gem 'rockstar', :git => 'git://github.com/markturner/rockstar.git'
+gem 'hpricot'
+gem 'i18n'
+gem 'json'
+gem 'activesupport'
--- app.rb
+++ app.rb
@@ -3,7 +3,46 @@ require 'bundler'
 Bundler.setup
 
 require 'sinatra'
+require 'hpricot'
+require 'rockstar'
+require 'json'
+require 'active_support'
+
+configure do  
+  # set last update time
+  @@last_updated = Time.now
+  
+  # initialise api key
+  Rockstar.lastfm = YAML.load_file('lastfm.yml')
+
+  # get my user id
+  @@user = Rockstar::User.new('markturner')
+  
+  # get albums
+  @@albums = @@user.weekly_album_chart
+  
+end
 
 get '/' do
-  'Hello world!'
+  array = []
+  
+  @@albums.each do |a|
+    info = a.load_info
+    
+    # need to come up with a way of getting track count, as this has been removed from last.fm api!
+    
+    # pushes played albums to an array
+    if a.playcount.to_i >= 7
+      array << {
+        :title => a.name,
+        :artist => a.artist,
+        :url => info[:url],
+        :image_url => info[:image_url]
+      }
+    end
+  end
+  
+  # return array as json object
+  array.to_json
+  
 end
